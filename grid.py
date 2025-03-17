cc4ba0722eb71c48346634345bc6e9d995ccbdac
Isaiah Estrada <isaiahestrada@Isaiahs-MacBook-Air.local>
2025-03-17 14:51:39 -0400

    add grid.py

---
+++ grid.py
@@ -0,0 +1,12 @@
+grid = []
+computergrid=[]
+for i in range(10)
+    grid.append([])
+for row in range(10):
+    row.append("X")
+
+carrierSize = 5
+battleshipSize = 4
+cruiserSize = 3 
+SubarmineSize = 3
+DestroyerSize = 2
